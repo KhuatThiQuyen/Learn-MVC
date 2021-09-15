92c7b9981d4e6df71a1dd95ee1384c203e93e7a2
quyen.kt175653@sis.hust.edu.vn <quyen.kt175653@sis.hust.edu.vn>
2021-09-15 17:54:12 +0700

    change type show score

--- Models/ScoreCalculator.cs
+++ Models/ScoreCalculator.cs
@@ -7,23 +7,23 @@ namespace StudentManagerMVC.Models
 {
     public class ScoreCalculator
     {
-        //private school_managementEntities db;
-        //public ScoreCalculator(double firstScore, double secondScore, double thirdScore,  Subject subject)
+        //private school_managemententities db;
+        //public scorecalculator(double firstscore, double secondscore, double thirdscore, subject subject)
         //{
-        //    this.firstScore = firstScore;
-        //    this.secondScore = secondScore;
-        //    this.thirdScore = thirdScore;  
+        //    this.firstscore = firstscore;
+        //    this.secondscore = secondscore;
+        //    this.thirdscore = thirdscore;
         //    this.subject = subject;
-        //    db = new school_managementEntities();
+        //    db = new school_managemententities();
 
         //}
-      
-        public Subject subject { get; set; }
 
+        public Subject subject { get; set; }
         public List<double> firstScore { get; set; }
         public List<double> secondScore { get; set; }
         public List<double> thirdScore { get; set; }
         public double finalScore { get; set; } // custom get method to return finalscore = 1 x fiS + 2 x seS + 3 x thS
 
+        
     }
 }
